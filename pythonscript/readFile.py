8bcd3ca5f410484cf9677ec96c70489d925929e4
Martin Ziel <Ziel.Martin@gmail.com>
2017-11-14 16:40:22 +0100

    out.json = aufgeräumte autoliste

--- pythonscript/readFile.py
+++ pythonscript/readFile.py
@@ -1,32 +1,41 @@
 import json
+import re as RegEx
+
+class Cars:
+    def __init__(self):
+        self.cars = []
+
+    def append(self, car):
+        self.cars.append(car)
 
 class Car:
-    def __init__(self, kh10t, tk10t, vk10t, name, baujahre, ps, cm3, kraftstoff, hsntsn):
-        self.kh10t = kh10t
-        self.tk10t = tk10t
-        self.vk10t = vk10t
-        self.name = name
-        self.baujahre = baujahre
-        self.ps = ps
-        self.cm3 = cm3
-        self.kraftstoff = kraftstoff
-        self.hsntsn = hsntsn
+    def __init__(self, j):
+        self.__dict__ = j
 
-    def __str__(self):
-        return "{ kh10t: " + self.kh10t + \
-        " tk10t: " + self.tk10t + \
-        " vk10t: " + self.vk10t + \
-        " name: " + self.name + \
-        " baujahre: " + self.baujahre + \
-        " ps: " + self.ps + \
-        " cm3: " + self.cm3 + \
-        " kraftstoff: " + self.kraftstoff + \
-        "hsntsn: " + self.hsntsn + " }"
+    def CLEANME(self):
+        pattern = RegEx.compile("\['(.*)'\]")
+        for eintrag in self.__dict__:
+            result = pattern.match(self.__dict__[eintrag])
+            if result:
+                self.__dict__[eintrag] = result.group(1)
 
+        result = RegEx.match("(.*)\s?/\s?(.*)", self.__dict__["hsntsn"])
+        self.hsn = result.group(1)
+        self.tsn = result.group(2)
 
+        del self.__dict__["hsntsn"]
+        del self.__dict__["kh10t"]
+        del self.__dict__["tk10t"]
+        del self.__dict__["vk10t"]
 
-with open("ca.rs", "r") as fp:
-    for x in json.loads(fp.read()):
-        car = Car(x["kh10t"], x["tk10t"], x["vk10t"], x["name"], x["baujahre"], x["ps"], x["cm3"], x["kraftstoff"], x["hsntsn"])
-        print(car)
-        input()
+def obj_dict(obj):
+    return obj.__dict__
+
+with open("ca.rs", "r") as inp:
+    with open("out.json", "w") as out:
+        cars = Cars()
+        for x in json.loads(inp.read()):
+            car = Car(x)
+            car.CLEANME()
+            cars.append(car)
+        json.dump(cars, out, default=obj_dict)
