ff9c3d529395dbd8732c9db72961e641b1ccc92c
Martin Ziel <Ziel.Martin@gmail.com>
2017-11-26 12:49:51 +0100

    simple Login/Logout removed annoying .idea files gradle.xml and misc.xml

--- dev/android/Carlogger/app/src/main/java/de/fhbi/mobappproj/carlogger/MainActivity.java
+++ dev/android/Carlogger/app/src/main/java/de/fhbi/mobappproj/carlogger/MainActivity.java
@@ -1,74 +1,69 @@
 package de.fhbi.mobappproj.carlogger;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private static final int RC_SIGN_IN = 123;
+    private static final String TAG = "MainActivity";
 
-    private FirebaseAuth mAuth;
+    // Choose authentication providers
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build());
 
-    private FirebaseAuth.AuthStateListener mAuthListener;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Log.i(TAG, "user logged in: " + user.getDisplayName());
+                // ...
+            } else {
+                // Sign in failed, check response for error code
+                Log.i(TAG, "login failed. response: " + response.toString());
+            }
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mAuth = FirebaseAuth.getInstance();
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Log.d("TAG", "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    // User is signed out
-                    Log.d("TAG", "onAuthStateChanged:signed_out");
-                }
-            }
-        };
-
-
-
-
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn(view);
-            }
-        });
+        fab.setOnClickListener((view) -> signIn());
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -83,36 +78,37 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onStart() {
         super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
     }
 
-    public void signIn(final View view) {
-        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                Log.d("TAG", "anonSignin:onComplete:" + task.isSuccessful());
+    private void signIn() {
+        // Create and launch sign-in intent
+        Intent signinIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
 
-                // If sign in fails, display a message to the user. If sign in succeeds
-                // the auth state listener will be notified and logic to handle the
-                // signed in user can be handled in the listener.
-                if (!task.isSuccessful()) {
-                    Log.w("TAG", "signInWithEmail:failed", task.getException());
-                    Toast.makeText(MainActivity.this, R.string.common_signin_button_text,
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    Snackbar.make(view, mAuth.getCurrentUser().getUid(), Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
+        startActivityForResult(signinIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        AuthUI authUI = AuthUI.getInstance();
+
+        Task logoutTask = authUI.signOut(this);
+
+        logoutTask.addOnCompleteListener(new OnCompleteListener<Void>() {
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful())
+                    Log.i(TAG, "user logged out: " + user.getDisplayName());
             }
         });
+
     }
 
     @Override
@@ -165,6 +161,15 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
+        } else if (id == R.id.loginlogout) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if(user == null) {
+                signIn();
+                item.setTitle("Logout");
+            } else {
+                signOut();
+                item.setTitle("Login");
+            }
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
